a8b446c0869da1fda9295dcdf4ac0881f6bb201f
송현서 <songmin21s@songhyeonseoui-MacBookPro.local>
2024-12-17 21:54:23 +0900

    asdf

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -7,4 +7,10 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+if (!supabaseUrl || !supabaseKey) {
+  return {
+    notFound: true,
+  };
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
